25a5e93efac70805e3c0cb2c861f3a44caba0e45
Ahtaxam <ahtaxam03@gmail.com>
2024-06-07 17:21:11 +0500

    add getMessages route endpoint

--- routes/message.js
+++ routes/message.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const route = express.Router();
 
-const {sendMessage} = require("../controller/message");
+const {sendMessage,getMessages} = require("../controller/message");
 const isAuth = require("../middleware/isAuth") 
 
-route.post('/send/:id', isAuth, sendMessage)
+route.post('/send/:id', isAuth, sendMessage);
+route.get('/:id', isAuth, getMessages)
 
 module.exports = route
