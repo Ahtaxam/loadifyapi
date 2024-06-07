edf69811cb45d44451a05c5758448f16bbccaa31
Ahtaxam <ahtaxam03@gmail.com>
2024-06-07 17:21:35 +0500

    implement getMessages controller

--- controller/message.js
+++ controller/message.js
@@ -29,15 +29,33 @@ const sendMessage = async (req, res) => {
       receiverId,
       message,
     });
-    await newMessage.save();
 
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
 
-    await conversation.save();
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(200).json(newMessage);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    }).populate('messages');
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(conversation.messages);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -51,4 +69,4 @@ const validateMessage = (data) => {
   return schema.validate(data);
 };
 
-module.exports = { sendMessage };
+module.exports = { sendMessage, getMessages };
