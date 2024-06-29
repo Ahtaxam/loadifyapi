b5e5d62655c35954df20c104e014b2c89720ef0f
ahtasham <ahtaxam03@gmail.com>
2024-06-29 13:35:52 +0500

    email handler method

---
+++ utils/emailHandler.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer")
+
+const handleEmail = async (to, subject, text) => {
+  try {
+    const mailTransporter =  nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SENDER_MAIL_ADDRESS,
+        pass: process.env.SENDER_MAIL_PASSWORD
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.SENDER_MAIL_ADDRESS,
+      to,
+      subject,
+      text,
+    };
+
+    const result = await mailTransporter.sendMail(mailOptions);
+    return result.messageId;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+module.exports = handleEmail
