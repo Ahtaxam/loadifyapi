4a6e3630b5061815df82a677348201f7262d7abd
Ahtaxam <ahtaxam03@gmail.com>
2024-07-16 09:11:30 +0500

    improve api response time

--- controller/getRecord.js
+++ controller/getRecord.js
@@ -5,83 +5,78 @@ const Booking = require("../schema/booking");
 
 const getTotalRecord = async (req, res) => {
   try {
-    const totalInventories = await INVENTORYADD.countDocuments();
-    const totalLoader = await vehicleAdd.countDocuments();
-    const totalusers = await User.countDocuments();
-    const bookedOrder = await Booking.countDocuments();
-
-    // get loaders stateName and their ratio
-    const loader = await vehicleAdd.aggregate([
-      {
-        $group: {
-          _id: "$stateName",
-          count: { $sum: 1 },
+    const [
+      totalInventories,
+      totalLoader,
+      totalUsers,
+      bookedOrder,
+      loader,
+      inventory,
+      loaderCityRatio,
+      inventoryCityRatio,
+    ] = await Promise.all([
+      INVENTORYADD.countDocuments(),
+      vehicleAdd.countDocuments(),
+      User.countDocuments(),
+      Booking.countDocuments(),
+      vehicleAdd.aggregate([
+        {
+          $group: {
+            _id: "$stateName",
+            count: { $sum: 1 },
+          },
         },
-      },
+      ]),
+      INVENTORYADD.aggregate([
+        {
+          $group: {
+            _id: "$stateName",
+            count: { $sum: 1 },
+          },
+        },
+      ]),
+      vehicleAdd.aggregate([
+        {
+          $group: {
+            _id: "$city",
+            count: { $sum: 1 },
+          },
+        },
+      ]),
+      INVENTORYADD.aggregate([
+        {
+          $group: {
+            _id: "$city",
+            count: { $sum: 1 },
+          },
+        },
+      ]),
     ]);
 
-    const registeredLoaders = loader.map((obj) => {
-      return {
-        label: obj._id,
-        value: obj.count,
-      };
-    });
+    const registeredLoaders = loader.map((obj) => ({
+      label: obj._id,
+      value: obj.count,
+    }));
 
-    // get registered inventories and their ratio
-    const inventory = await INVENTORYADD.aggregate([
-      {
-        $group: {
-          _id: "$stateName",
-          count: { $sum: 1 },
-        },
-      },
-    ]);
+    const registeredInventories = inventory.map((obj) => ({
+      label: obj._id,
+      value: obj.count,
+    }));
 
-    const registeredInventories = inventory.map((obj) => {
-      return {
-        label: obj._id,
-        value: obj.count,
-      };
-    });
+    const registeredLoadersCityRation = loaderCityRatio.map((obj) => ({
+      label: obj._id,
+      value: obj.count,
+    }));
 
-    // get loaders city ratio
-    const loaderCityRatio = await vehicleAdd.aggregate([
-      {
-        $group: {
-          _id: "$city",
-          count: { $sum: 1 },
-        },
-      },
-    ]);
-
-    const registeredLoadersCityRation = loaderCityRatio.map((obj) => {
-      return {
-        label: obj._id,
-        value: obj.count,
-      };
-    });
-
-    // get inventory city ratio
-    const inventoryCityRatio = await INVENTORYADD.aggregate([
-      {
-        $group: {
-          _id: "$city",
-          count: { $sum: 1 },
-        },
-      },
-    ]);
-
-    const registeredInventoriesCityRation = inventoryCityRatio.map((obj) => {
-      return {
-        label: obj._id,
-        value: obj.count,
-      };
-    });
+    const registeredInventoriesCityRation = inventoryCityRatio.map((obj) => ({
+      label: obj._id,
+      value: obj.count,
+    }));
 
     const totalRecord = {
       inventory: totalInventories,
       loaders: totalLoader,
-      users: totalusers,
+      users: totalUsers,
       booking: bookedOrder,
       loadersData: registeredLoaders,
       inventoryData: registeredInventories,
@@ -90,7 +85,7 @@ const getTotalRecord = async (req, res) => {
     };
 
     res.status(200).json({
-      message: "total record fetched successfully",
+      message: "Total record fetched successfully",
       data: totalRecord,
       status: 200,
     });
